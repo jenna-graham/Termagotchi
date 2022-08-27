dff60b96628172c177554f92214235d52d07bc24
jenna-graham <jclayto2@gmail.com>
2022-08-26 17:11:14 -0700

    initial db table setup

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,32 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS actions;
+DROP TABLE IF EXISTS user_actions;
+
+CREATE TABLE users (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    username VARCHAR NOT NULL
+);
+
+CREATE TABLE actions (
+    id BIGINT PRIMARY KEY,
+    prompt VARCHAR NOT NULL,
+    happy_choice VARCHAR NOT NULL,
+    neglect_choice VARCHAR NOT NULL,
+    happy_path_id INT,
+    neglect_path_id INT,
+    is_good BOOLEAN
+);
+
+CREATE TABLE user_actions (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    path_id BIGINT,
+    user_id BIGINT,
+    pet_owner BOOLEAN,
+    FOREIGN KEY (path_id) REFERENCES actions(id),
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+INSERT INTO actions (id, prompt, happy_choice, neglect_choice, happy_path_id, neglect_path_id, is_good) VALUES
+(1, 'Congratulations on your new pet! You will want to take care of them so they will love you back (neglected pets never stay too long)', 'smile and say hello', 'go about your busy life', 2, 3, null);
