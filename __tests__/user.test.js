29acb20cd7352717d9fbb2ae1bba236ab98c8c27
Mariah <mariahschock@gmail.com>
2022-08-29 11:00:01 -0700

    our post route to create new users test is passing

---
+++ __tests__/user.test.js
@@ -0,0 +1,28 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+
+const mockUser = {
+  username: 'Cupcake',
+  password: '123456',
+};
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+ 
+  it('POST /users should create new user', async () => {
+    const res = await request(app).post('/users').send(mockUser);
+    const { username } = mockUser;
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      username,
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,12 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS users CASCADE;
-DROP TABLE IF EXISTS actions;
+DROP TABLE IF EXISTS actions CASCADE;
 DROP TABLE IF EXISTS user_actions;
 
 CREATE TABLE users (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    username VARCHAR NOT NULL
+    username VARCHAR NOT NULL,
+    password_hash VARCHAR NOT NULL 
 );
 
 CREATE TABLE actions (
@@ -23,10 +24,13 @@ CREATE TABLE user_actions (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     path_id BIGINT,
     user_id BIGINT,
-    pet_owner BOOLEAN,
+    -- pet_owner BOOLEAN,
     FOREIGN KEY (path_id) REFERENCES actions(id),
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
+
+INSERT INTO users(username, password_hash) VALUES
+('Cupcake', '123456');
 
 INSERT INTO actions (id, prompt, happy_choice, neglect_choice, happy_path_id, neglect_path_id, is_good) VALUES
 (1, 'Congratulations on your new pet! You will want to take care of them so they will love you back (neglected pets never stay too long)', 'smile and say hello', 'go about your busy life', 2, 3, null);
