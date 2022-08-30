805542dbe6f9679654a714d83888e25c0c1c25c5
jenna-graham <jclayto2@gmail.com>
2022-08-30 09:52:20 -0700

    playing with inquirer

--- __tests__/action.test.js
+++ __tests__/action.test.js
@@ -4,24 +4,24 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('testing action routes', () => {
-    beforeEach(() => {
-      return setup(pool);
-    });
-it('GET /actions/:id should return a prompt and two options by id', async () => {
-    const res = await request(app).get('/actions/1')
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET /actions/:id should return a prompt and two options by id', async () => {
+    const res = await request(app).get('/actions/1');
     expect(res.body).toEqual({
-        id: expect.any(String),
-        prompt:'Congratulations on your new pet! You will want to take care of them so they will love you back (neglected pets never stay too long)',
-        happy_choice: 'smile and say hello',
-        neglect_choice: 'go about your busy life',
-        happy_path_id: 2,
-        neglect_path_id: 3,
-        is_good: null,
-    })
-});
-
-
-    afterAll(() => {
-        pool.end();
-      });
+      id: expect.any(String),
+      prompt:
+        'Congratulations on your new pet! You will want to take care of them so they will love you back (neglected pets never stay too long)',
+      happy_choice: 'smile and say hello',
+      neglect_choice: 'go about your busy life',
+      happy_path_id: 2,
+      neglect_path_id: 3,
+      is_good: null,
     });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
--- index.js
+++ index.js
@@ -3,45 +3,54 @@ require('dotenv').config();
 const { signUpUser, getPromptsById } = require('./lib/utils/utils');
 const chalk = require('chalk');
 const inquirer = require('inquirer');
+const { prompt, happy_choice, neglect_choice } = getPromptsById(1);
 
 const setUser = async () => {
   inquirer
-    .prompt([{
-      prefix: '*',
-      name: 'username',
-      message: 'Name your Termagotchi!',
-    },
-    {
-      prefix: '*',
-      name: 'password',
-      type: 'password',
-      message: 'Enter your password',
-    },
+    .prompt([
+      {
+        prefix: '*',
+        name: 'username',
+        message: 'Name your Termagotchi!',
+      },
+      {
+        prefix: '*',
+        name: 'password',
+        type: 'password',
+        message: 'Enter your password',
+      },
     ])
     .then((answers) => {
-      console.log(
-        chalk.bold(
-          `Say hi to ${answers.username}!`
-        )
-      );
+      console.log(chalk.bold(`Say hi to ${answers.username}!`));
       return signUpUser(answers.username, answers.password);
-
     })
     .then((user) => {
-      storyLine(1, user);
+      storyLine(user);
     });
-
 };
 
-const storyLine = async (storyId) => {
-  let currentPrompts = {};
-  getPromptsById(storyId)
-    .then((prompts) => {
-      currentPrompts = prompts;
-      return getPromptsById(prompts.neglect_path_id);
-
+const storyLine = () => {
+  inquirer
+    .prompt([
+      {
+        name: 'Question',
+        type: 'list',
+        message: 'Do you want to walk?',
+        choices: ['yes', 'no'],
+      },
+    ])
+    .then((answers) => {
+      console.log(answers.Question);
     });
-
 };
-    
+
+// const storyLine = async (storyId) => {
+//   let currentPrompts = {};
+//   getPromptsById(storyId)
+//     .then((prompts) => {
+//       currentPrompts = prompts;
+//       return getPromptsById(prompts.neglect_path_id);
+
+//     });
+
 setUser();
