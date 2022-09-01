09abed0ce04d50dbe4036fcbad3aff3f154a3542
jenna-graham <jclayto2@gmail.com>
2022-09-01 15:44:37 -0700

    updated test

--- __tests__/action.test.js
+++ __tests__/action.test.js
@@ -8,16 +8,17 @@ describe('testing action routes', () => {
     return setup(pool);
   });
   it('GET /actions/:id should return a prompt and two options by id', async () => {
-    const res = await request(app).get('/actions/1');
+    const res = await request(app).get('/actions/2');
     expect(res.body).toEqual({
       id: expect.any(String),
-      prompt: 'Your Termagotchi is home with you and looks bored',
+      prompt:
+        'You are walking down the street when you notice a forested trail.',
       happy_choice:
-        'Take your Termagotchi outside and watch the adventure unfold.',
+        'This looks Exciting, so you take your Termagotchi on a walk through the forest.',
       neglect_choice:
-        'Walk to the living room and turn on the TV. You are tired and have no time to play.',
-      happy_path_id: 2,
-      neglect_path_id: 3,
+        'There is no time for new adventures! Continue walking your normal route.',
+      happy_path_id: 4,
+      neglect_path_id: 5,
     });
   });
 
